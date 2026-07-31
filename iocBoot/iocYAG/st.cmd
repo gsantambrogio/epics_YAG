2a90002e064fdacf1198c42bbeb2dcae9aea1bb2
Gabriele Santambrogio <santambrogio@lens.unifi.it>
2026-07-31 13:20:15 +0200

    Split off YAG app from lasersIOC
    Renamed lasersIOC -> YAG (owns the YAG laser only, FLM0151.db/.proto
    carried over unchanged). Pruned unused demo/example/Toptica files (kept
    xxxRecord/xxxSupport/devXxxSoft -- load bearing for asyn registration
    despite generic names).

---
+++ iocBoot/iocYAG/st.cmd
@@ -0,0 +1,35 @@
+#!../../bin/linux-arm/YAG
+
+## You may have to change YAG to something else
+## everywhere it appears in this file
+
+< envPaths
+
+epicsEnvSet("STREAM_PROTOCOL_PATH","$(TOP)/db")
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/YAG.dbd"
+YAG_registerRecordDeviceDriver pdbbase
+
+# streamDevice protocol file location
+epicsEnvSet("STREAM_PROTOCOL_PATH","$(TOP)/db")
+
+#DB include for database concatenation
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(TOP)/db")
+
+#YAG
+drvAsynSerialPortConfigure("YAG","/dev/YAG",0,0,0)
+asynSetOption("YAG", -1, "baud", "57600")
+asynSetOption("YAG", -1, "bits", "8")
+asynSetOption("YAG", -1, "parity", "none")
+asynSetOption("YAG", -1, "stop", "1")
+
+dbLoadRecords("db/FLM0151.db", "P=pi:,PORT=YAG,R=YAG:,A=0")
+
+## Run this to trace the stages of iocInit
+#traceIocInit
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
